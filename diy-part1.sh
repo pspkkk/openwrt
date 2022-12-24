d5d7af567f17fd019358b59f3af859cf96663b99
pspkkk <121186454+pspkkk@users.noreply.github.com>
2022-12-25 00:06:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -76,7 +76,7 @@ git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/luci
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 
 #luci-app-adguardhome
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 
 ##############################################################################################
 
